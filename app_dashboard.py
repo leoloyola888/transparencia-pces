f6e07e3481f3a12f0147b48bc992062aece40bd6
leoloyola888 <59941489+leoloyola888@users.noreply.github.com>
2026-08-02 19:05:21 -0300

    Update app_dashboard.py

--- app_dashboard.py
+++ app_dashboard.py
@@ -55,7 +55,7 @@ with col_logo:
 
 with col_texto:
     st.markdown('<p class="main-title">DIRETORIA DE INTELIGÊNCIA E DADOS - PCES</p>', unsafe_allow_html=True)
-    st.markdown('<p class="sub-title">Painel Estratégico da Comissão de Aprovados (Investigador)</p>', unsafe_allow_html=True)
+    st.markdown('<p class="sub-title">Painel Estratégico dos Aprovados (Investigador)</p>', unsafe_allow_html=True)
 
 # =============================================================
 # 3. MOTOR DE DADOS - LEITURA OFFLINE RÁPIDA
